fab74b9a8f2b27d6a02df8a5dbb8be57c374d00b
Akashnaidu798 <80052658+Akashnaidu798@users.noreply.github.com>
2021-03-07 21:05:06 +0530

    Create statusChecker.sh

---
+++ statusChecker.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+if curl -s --head --request GET http://localhost/version.txt | grep "200 OK" > /dev/null; then
+if curl -s GET http://localhost/version.txt | grep "1.0.6" > /dev/null; then
+   echo "Webpage is UP and serving the content 1.0.6 on "`date` > /home/ec2-user/status.txt
+else
+echo "Webpage is UP and not serving the content 1.0.6 on "`date` > /home/ec2-user/status.txt
+fi
+else
+echo "Webpage is DOWN on  " `date` > /home/ec2-user/status.txt
+fi
