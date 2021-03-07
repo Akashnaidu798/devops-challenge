bcab68967ebc63880932ffb683d4efd2bc726021
Akashnaidu798 <80052658+Akashnaidu798@users.noreply.github.com>
2021-03-07 22:01:28 +0530

    Update statusChecker.sh

--- statusChecker.sh
+++ statusChecker.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-if curl -s --head --request GET http://localhost/version.txt | grep "200 OK" > /dev/null; then
-if curl -s GET http://localhost/version.txt | grep "1.0.6" > /dev/null; then
+if curl -s --head --request GET http://nginxversioncheck-409897852.us-east-2.elb.amazonaws.com/version.txt | grep "200 OK" > /dev/null; then
+if curl -s GET http://nginxversioncheck-409897852.us-east-2.elb.amazonaws.com/version.txt | grep "1.0.6" > /dev/null; then
    echo "Webpage is UP and serving the content 1.0.6 on "`date` > /home/ec2-user/status.txt
 else
 echo "Webpage is UP and not serving the content 1.0.6 on "`date` > /home/ec2-user/status.txt
